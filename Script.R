7b2ec67c1835f95fb9320fb5bfc0da5e9a9a9bb2
hermanndavidcode <hdospinan@gmail.com>
2020-11-27 11:16:16 -0500

    Código para calcular diferencias porcentuales entre dos años.
    El resultado reporta: diferencia porcentual, diferencia en casos, y la tendencia de la variación.

--- Script.R
+++ Script.R
@@ -1,3 +1,15 @@
-print("This file was created within RStudio")
+varpor <- function(añoant, añoact) {
+        añoant <- sum(añoant)
+        añoact <- sum(añoact)
+        diferencia <- añoact-añoant
+        difporcent <- diferencia*100/añoant
+        texto <- paste0("Variación %: ", difporcent,", ", "Número de casos: ", 
+                        diferencia, ", ", "Tendencia: ")
+        if(difporcent<0){
+                print(paste0(texto, "Disminuyó"))
+        } else {
+                print(paste0(texto, "Aumentó"))
+        } 
+}
 
-print("And now it lives on GitHub")
+varpor(20,1)
